5ebcf9b9472acabcad22a43bf9ccd644f57f1ece
Gabriel L. Maljkovich <gabriel.l.maljkovich@gmail.com>
2024-04-12 18:07:11 -0300

    cubes

---
+++ app/components/Scene.jsx
@@ -0,0 +1,23 @@
+'use client';
+import { Canvas } from '@react-three/fiber'
+import { Fisheye, OrbitControls, CameraControls, PerspectiveCamera, Environment } from '@react-three/drei'
+import { Cubes, Camera, Light1, Light2 } from '@/app/components/Cubes'
+import { Loader } from '@react-three/drei'
+
+export default function Scene() {
+    return (
+        <>
+        <Loader />
+        <Canvas>
+        <CameraControls distance={20}/>
+        <Camera />
+                <Cubes></Cubes>
+
+                <Light1 />
+                <Light2 />
+
+            
+        </Canvas>
+    </>
+    )
+  }
