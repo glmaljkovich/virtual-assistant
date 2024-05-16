ae2ca67ad3fbcf6e5471008b8bcbbe8d066013a2
Gabriel L. Maljkovich <gabriel.l.maljkovich@gmail.com>
2024-05-16 11:12:09 -0300

    speech recognition

--- components/CharacterScene.jsx
+++ components/CharacterScene.jsx
@@ -1,106 +1,119 @@
-import {CameraControls, OrbitControls, PerspectiveCamera, Grid} from "@react-three/drei";
+import {
+  CameraControls,
+  OrbitControls,
+  PerspectiveCamera,
+  Grid,
+} from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 
 import { Stars } from "./Cubes";
 import { Suspense, useEffect, useState, useRef } from "react";
 import { Object3D } from "three";
 import { Leva } from "leva";
+import { LuRefreshCcw } from "react-icons/lu";
 
-import { Character } from "@/components/vrm/Character"
-import Chat from "@/components/Chat"
+import { Character } from "@/components/vrm/Character";
+import Chat from "@/components/Chat";
 
-export function Camera1(props = {default: boolean = true, lookAt: Object3D}) {
-    const camera = useRef()
-    useEffect(() => {
-        if (props.lookAt) {
-            camera.current.add(props.lookAt.current)
-        }
-    }, [props.lookAt])
+export function Camera1(
+  props = { default: (boolean = true), lookAt: Object3D },
+) {
+  const camera = useRef();
+  useEffect(() => {
+    if (props.lookAt) {
+      camera.current.add(props.lookAt.current);
+    }
+  }, [props.lookAt]);
 
-    return (
-        <PerspectiveCamera
-            name="Camera1"
-            makeDefault={true}
-            position={[0,1.7,3]}
-            fov={22}
-            rotation={[-0.1,0,0]}
-            ref={camera}
-        />
-    )
+  return (
+    <PerspectiveCamera
+      name="Camera1"
+      makeDefault={true}
+      position={[0, 1.7, 3]}
+      fov={22}
+      rotation={[-0.1, 0, 0]}
+      ref={camera}
+    />
+  );
 }
 
 function Ground() {
-    const gridConfig = {
-      cellSize: 0.75,
-      cellThickness: 0.6,
-      cellColor: '#6f6f6f',
-      sectionSize: 3,
-      sectionThickness: 1,
-      sectionColor: '#9d4b4b',
-      fadeDistance: 30,
-      fadeStrength: 1.5,
-      followCamera: false,
-      infiniteGrid: true
-    }
-    return <Grid position={[0, -0.01, 0]} args={[10, 10]} {...gridConfig} />
-  }
+  const gridConfig = {
+    cellSize: 0.75,
+    cellThickness: 0.6,
+    cellColor: "#6f6f6f",
+    sectionSize: 3,
+    sectionThickness: 1,
+    sectionColor: "#9d4b4b",
+    fadeDistance: 30,
+    fadeStrength: 1.5,
+    followCamera: false,
+    infiniteGrid: true,
+  };
+  return <Grid position={[0, -0.01, 0]} args={[10, 10]} {...gridConfig} />;
+}
 
 export default function CharacterScene() {
-    const lookAt = useRef()
+  const lookAt = useRef();
 
-    const [fullText, setFullText] = useState("")
-    const [thinking, setThinking] = useState(false)
-    const [assistant, setAssistant] = useState('Illia')
-    const [emotion, setEmotion] = useState("")
+  const [fullText, setFullText] = useState("");
+  const [thinking, setThinking] = useState(false);
+  const [assistant, setAssistant] = useState("Illia");
+  const [emotion, setEmotion] = useState("");
 
-    return (
-        <div className="h-dvh w-full">
-            <div className="absolute top-0 z-10 left-0 text-center">
-                <div className=" text-xl mt-2 ml-2 w-auto z-10 uppercase tracking-wider font-bold text-white px-4 rounded-3xl bg-slate-700">
-                    {assistant}
-                    <span className="pointer " onClick={() => assistant === 'Illia' ? setAssistant('Amelia') : setAssistant('Illia') }>
-                        🔄
-                    </span>
-                </div>
-            </div>
-            <div className="relative h-full">
-                <Canvas flat className="canvi">
-                    <Camera1 lookAt={lookAt} />
-                    <OrbitControls
-                        target={[0,1.3,0]}
-                        maxDistance={5.5}
-                        minDistance={1}
-                        maxAzimuthAngle={Math.PI / 2}
-                        minAzimuthAngle={-Math.PI / 2}
-                        maxPolarAngle={Math.PI / 1.5}
-                        minPolarAngle={Math.PI / 4}
-                        enablePan={false}
-                        
-                    />
-                    <spotLight position={[0, 2, -1]} intensity={0.5} />
-                    <ambientLight intensity={0.6} />
-                    <Ground />
-                    <Stars />
-                    <Character
-                        lookAt={lookAt}
-                        text={fullText}
-                        thinking={thinking}
-                        assistant={assistant}
-                        emotion={emotion}
-                    />
-                    <object3D ref={lookAt}/>
-                </Canvas>
-                <Leva collapsed hidden/>
-                <div className="w-full px-4 md:w-1/3 bottom-6 md:left-1/3 absolute ">
-                    <Chat
-                        setText={setFullText}
-                        setThinking={setThinking}
-                        agentName={assistant}
-                        setEmotion={(emot) => setEmotion(emot)}
-                    />
-                </div>
-            </div>
-
+  return (
+    <div className="h-dvh w-full">
+      <div className="absolute top-0 z-10 left-0 text-center">
+        <div className=" text-xl mt-2 ml-2 w-auto z-10 uppercase tracking-wider font-bold text-white px-4 rounded-3xl bg-slate-800">
+          {assistant}
+          <span
+            className="pointer "
+            onClick={() =>
+              assistant === "Illia"
+                ? setAssistant("Amelia")
+                : setAssistant("Illia")
+            }
+          >
+            <LuRefreshCcw className="text-sm text-blue-300 inline-block ml-2" />
+          </span>
         </div>
-    )
-  }
+      </div>
+      <div className="relative h-full">
+        <Canvas flat className="canvi">
+          <Camera1 lookAt={lookAt} />
+          <OrbitControls
+            target={[0, 1.3, 0]}
+            maxDistance={5.5}
+            minDistance={1}
+            maxAzimuthAngle={Math.PI / 2}
+            minAzimuthAngle={-Math.PI / 2}
+            maxPolarAngle={Math.PI / 1.5}
+            minPolarAngle={Math.PI / 4}
+            enablePan={false}
+          />
+          <spotLight position={[0, 2, -1]} intensity={0.5} />
+          <ambientLight intensity={0.6} />
+          <Ground />
+          <Stars />
+          <Character
+            lookAt={lookAt}
+            text={fullText}
+            thinking={thinking}
+            assistant={assistant}
+            emotion={emotion}
+          />
+          <object3D ref={lookAt} />
+        </Canvas>
+        <Leva collapsed hidden />
+        <div className="w-full px-4 md:w-1/3 bottom-6 md:left-1/3 absolute ">
+          <Chat
+            setText={setFullText}
+            setThinking={setThinking}
+            agentName={assistant}
+            setEmotion={(emot) => setEmotion(emot)}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,67 +1,100 @@
-'use client';
- 
-import { useChat } from 'ai/react';
-import { AudioHTMLAttributes, ChangeEvent, Dispatch, FormEvent, SetStateAction, useEffect, useRef, useState } from 'react';
+"use client";
 
-const CHAT_ENDPOINT = '/api/chat'
+import { useChat, Message } from "ai/react";
+import classnames from "classnames";
+import {
+  AudioHTMLAttributes,
+  ChangeEvent,
+  Dispatch,
+  FormEvent,
+  SetStateAction,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { FaMicrophone } from "react-icons/fa";
+import { RxPaperPlane } from "react-icons/rx";
+
+const CHAT_ENDPOINT = "/api/chat";
 type ChatProps = {
-    setText: Dispatch<SetStateAction<string>>,
-    setThinking: Dispatch<SetStateAction<boolean>>,
-    agentName: string,
-    setEmotion: (emotion: string) => void
-}
+  setText: Dispatch<SetStateAction<string>>;
+  setThinking: Dispatch<SetStateAction<boolean>>;
+  agentName: string;
+  setEmotion: (emotion: string) => void;
+};
 
-export default function Chat({setText, setThinking, agentName, setEmotion}: ChatProps) {
+export default function Chat({
+  setText,
+  setThinking,
+  agentName,
+  setEmotion,
+}: ChatProps) {
   const player = useRef<HTMLAudioElement>(null);
-  const { messages, input, handleInputChange, handleSubmit, isLoading: chatEndpointIsLoading } = useChat({
+  const recognitionRef = useRef<SpeechRecognition | null>(null);
+  const [recording, setRecording] = useState(false);
+
+  const {
+    messages,
+    input,
+    handleInputChange,
+    handleSubmit,
+    append,
+    isLoading: chatEndpointIsLoading,
+  } = useChat({
     api: CHAT_ENDPOINT,
     body: {
-      agentName: agentName
+      agentName: agentName,
     },
     onFinish: async (message) => {
-      const emotionResponse = await fetch('/api/emotion', {method: 'POST', body: JSON.stringify({message: message.content})})
-      const emotion = await emotionResponse.text()
-      const audio = await getElevenLabsResponse(message.content)
-      setEmotion(emotion)
+      const emotionResponse = await fetch("/api/emotion", {
+        method: "POST",
+        body: JSON.stringify({ message: message.content }),
+      });
+      const emotion = await emotionResponse.text();
+      const audio = await getElevenLabsResponse(message.content);
+      setEmotion(emotion);
       const reader = new FileReader();
-      setThinking(false)
-      reader.readAsDataURL(audio)
+      setThinking(false);
+      reader.readAsDataURL(audio);
       reader.onload = () => {
         if (player.current) {
-            player.current.src = reader.result as string;
-            player.current.play();
-            setText(message.content)
-          }
+          player.current.src = reader.result as string;
+          player.current.play();
+          setText(message.content);
+        }
       };
     },
   });
-  const messagesEndRef = useRef<null | HTMLLIElement>(null)
+  const messagesEndRef = useRef<null | HTMLLIElement>(null);
 
   const scrollToBottom = () => {
-    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
-  }
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
 
   useEffect(() => {
-    scrollToBottom()
+    scrollToBottom();
   }, [messages]);
 
   const getElevenLabsResponse = async (text: string) => {
     const response = await fetch("/api/tts", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         text: text,
-        assistant: agentName
-      })
+        assistant: agentName,
+      }),
     });
 
     if (response.status === 401) {
-      console.log("Your ElevenLabs API Key is invalid. Kindly check and try again.", {
-        type: "error",
-        autoClose: 5000
-      });
+      console.log(
+        "Your ElevenLabs API Key is invalid. Kindly check and try again.",
+        {
+          type: "error",
+          autoClose: 5000,
+        },
+      );
     }
 
     const data = await response.blob();
@@ -69,60 +102,138 @@ export default function Chat({setText, setThinking, agentName, setEmotion}: Chat
   };
 
   const handlePrompt = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+    e.preventDefault();
     if (chatEndpointIsLoading) {
-        return
+      return;
     }
-    setThinking(true)
+    setThinking(true);
     const playThinkingSound = async () => {
       const reader = new FileReader();
-      const voiceMap = new Map(Object.entries({
-        'Illia': '/hmm_illia.mp3',
-        'Amelia': 'hmm_amelia.mp3'
-      }))
-      const audio = await (await fetch(voiceMap.get(agentName)!)).blob()
-      reader.readAsDataURL(audio)
+      const voiceMap = new Map(
+        Object.entries({
+          Illia: "/hmm_illia.mp3",
+          Amelia: "hmm_amelia.mp3",
+        }),
+      );
+      const audio = await (await fetch(voiceMap.get(agentName)!)).blob();
+      reader.readAsDataURL(audio);
       reader.onload = () => {
         if (player.current) {
-            player.current.src = reader.result as string;
-            player.current.play();
-          }
+          player.current.src = reader.result as string;
+          player.current.play();
+        }
       };
-    }
-    playThinkingSound()
-    handleSubmit(e)
-  }
+    };
+    playThinkingSound();
+    handleSubmit(e);
+  };
 
   const finishSpeaking = () => {
-    setText("")
-    setEmotion("")
-  }
- 
-  return (
-    <div className='flex flex-col gap-4'>
-      <ul className='flex flex-col gap-2 max-h-48 overflow-y-auto inner-shadow'>
-        {messages.map((m, index) => (
-          <li key={index} className='max-w-3/4 flex gap-4 items-center'>
-            {m.role === 'user' && <span className='text-slate-500 w-8'>You</span>}
-            {m.role !== 'user' && <span className='text-purple-300 w-8'>AI</span>}
-            <div className='bg-slate-900/80 rounded-xl p-4'>
-              {m.role === 'user' && <span className='text-slate-500'>{m.content}</span>}
-              {m.role !== 'user' && <span className='text-slate-300'>{m.content}</span>}
-            </div>
+    setText("");
+    setEmotion("");
+  };
 
+  useEffect(() => {
+    const speechRecognition =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
+    if (speechRecognition && !recognitionRef.current) {
+      recognitionRef.current = new speechRecognition();
+      recognitionRef.current.continuous = false;
+      recognitionRef.current.interimResults = false;
+
+      recognitionRef.current.onresult = (event) => {
+        const lastResultIndex = event.results.length - 1;
+        const transcriptResult = event.results[lastResultIndex][0].transcript;
+        append({ content: transcriptResult, role: "user" });
+        setRecording(false);
+        recognitionRef.current?.stop();
+      };
+
+      recognitionRef.current.onstart = () => {
+        setRecording(true);
+        console.log("start recognition");
+      };
+
+      recognitionRef.current.onnomatch = () => {
+        console.log("no match");
+      };
+
+      recognitionRef.current.onend = () => {
+        console.log("end recognition");
+      };
+    }
+  }, [recognitionRef, append]);
+
+  return (
+    <div className="flex flex-col gap-4 relative">
+      <ul className="flex flex-col gap-2 max-h-48 overflow-y-auto inner-shadow">
+        {messages.map((m, index) => (
+          <li key={index} className="max-w-3/4 flex gap-4 items-center">
+            {m.role === "user" && (
+              <span className="text-slate-500 w-8">You</span>
+            )}
+            {m.role !== "user" && (
+              <span className="text-purple-300 w-8">AI</span>
+            )}
+            <div className="bg-slate-900/80 rounded-xl p-4">
+              {m.role === "user" && (
+                <span className="text-slate-500">{m.content}</span>
+              )}
+              {m.role !== "user" && (
+                <span className="text-slate-300">{m.content}</span>
+              )}
+            </div>
           </li>
         ))}
         <li ref={messagesEndRef}></li>
       </ul>
- 
-      <form className='flex gap-4 w-full' onSubmit={handlePrompt}>
-          <audio ref={player} onEnded={finishSpeaking}/>
-          <input
-          placeholder='Ask me anything...'
+
+      <form className="flex gap-4 w-full" onSubmit={handlePrompt}>
+        <audio ref={player} onEnded={finishSpeaking} />
+        <span
+          className={classnames(
+            [
+              "rounded-full ",
+              "flex-shrink-0",
+              "bg-transparent ",
+              "border ",
+              "border-slate-200/50 ",
+              "text-blue-300 ",
+              "text-center ",
+              "w-7",
+            ],
+            {
+              "bg-red-500": recording,
+            },
+          )}
+          onTouchStart={() => {
+            recognitionRef.current?.start();
+            setRecording(true);
+          }}
+          onMouseDown={() => {
+            recognitionRef.current?.start();
+            setRecording(true);
+          }}
+          onMouseUp={() => {
+            recognitionRef.current?.stop();
+            setRecording(false);
+          }}
+          onTouchEnd={() => {
+            recognitionRef.current?.stop();
+            setRecording(false);
+          }}
+        >
+          <FaMicrophone className="inline-flex" />
+        </span>
+        <input
+          placeholder="Ask me anything..."
           value={input}
-          className="inline flex-shrink w-full bg-slate-800 text-xl shadow-lg px-2 rounded-xl text-slate-200 border-0 focus:border-0 outline-sky-500" 
-          onChange={handleInputChange} />
-        <button className='inline' type="submit">Send</button>
+          className="inline flex-shrink w-full bg-slate-800 text-xl shadow-lg px-2 rounded-xl text-slate-200 border-0 focus:border-0 outline-sky-500"
+          onChange={handleInputChange}
+        />
+        <button className="inline" type="submit">
+          <RxPaperPlane />
+        </button>
       </form>
     </div>
   );
